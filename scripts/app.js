1ee583cf46204b7fd2c764bf2021fe20a313e538
sethdame <seth.e.dame@gmail.com>
2015-10-21 23:59:26 -0600

    second commit

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,24 @@
-angular.module('myApp', [
-  'ngRoute', 
-  'myApp.services', 
-  'myApp.directives'])
-.config(function($routeProvider) {
-  $routeProvider
-  .when('/', {
-    controller: 'MainCtrl',
-    templateUrl: 'templates/main.html',
-  })
-  .otherwise({
-    redirectTo: '/'
-  });
+var amazonApp = angular.module('amazonApp', ['ngRoute','amazonApp.services','amazonApp.directives'])
+
+    amazonApp.config(function($routeProvider) {
+
+            $routeProvider
+                .when('/', {
+                      controller: 'MainCtrl',
+                      templateUrl: 'templates/main.html',
+                })
+                .otherwise({
+                      redirectTo: '/'
+                });
+
+            window.onLoadCallback = function() {
+                 // When the document is ready
+              angular.element(document).ready(function() {
+                    // Bootstrap the oauth2 library
+                  gapi.client.load('oauth2', 'v2', function() {
+                      // Finally, bootstrap angular app
+                      angular.bootstrap(document, ['myApp']);
+                          });
+                    });
+              }
 });
