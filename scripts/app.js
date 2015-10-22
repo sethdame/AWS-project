e3c2c0f4482f2d483ae07f6554374069e1bb1d7b
sethdame <seth.e.dame@gmail.com>
2015-10-21 23:48:01 -0600

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,14 @@
+angular.module('myApp', [
+  'ngRoute', 
+  'myApp.services', 
+  'myApp.directives'])
+.config(function($routeProvider) {
+  $routeProvider
+  .when('/', {
+    controller: 'MainCtrl',
+    templateUrl: 'templates/main.html',
+  })
+  .otherwise({
+    redirectTo: '/'
+  });
+});
