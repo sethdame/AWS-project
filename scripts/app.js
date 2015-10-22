9196ccd6db4147cdb0e502450825092218c15d74
sethdame <seth.e.dame@gmail.com>
2015-10-22 03:19:24 -0600

    directive added

--- scripts/app.js
+++ scripts/app.js
@@ -1,24 +1,19 @@
-var amazonApp = angular.module('amazonApp', ['ngRoute','amazonApp.services','amazonApp.directives'])
-
-    amazonApp.config(function($routeProvider) {
-
-            $routeProvider
-                .when('/', {
-                      controller: 'MainCtrl',
-                      templateUrl: 'templates/main.html',
-                })
-                .otherwise({
-                      redirectTo: '/'
-                });
-
-            window.onLoadCallback = function() {
-                 // When the document is ready
-              angular.element(document).ready(function() {
-                    // Bootstrap the oauth2 library
-                  gapi.client.load('oauth2', 'v2', function() {
-                      // Finally, bootstrap angular app
-                      angular.bootstrap(document, ['myApp']);
-                          });
-                    });
-              }
-});
+var amazonApp = angular.module("amazonApp", ['ngRoute', 'amazonApp.services', 'amazonApp.directives'])
+	
+	amazonApp.config(function($routeProvider) {
+		$routeProvider
+			.when('/', {
+				controller: 'MainCtrl',
+				templateUrl: 'templates/main.html'
+			})
+			.otherwise({
+				redirectTo: '/'
+			});
+		window.onLoadCallback = function() {
+			angular.element(document).ready(function() {
+				gapi.client.load('oauth2', 'v2', function() {
+					angular.bootstrap(document, ['amazonApp'])
+				});
+			});
+		}
+	});
